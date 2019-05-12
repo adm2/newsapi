d06f41080821706b7ca033ca16ac53b87fd12d62
Sergey Pashkov <pashkov.s.y@gmail.com>
2019-05-12 14:12:20 +0500

    pass

--- sorted.py
+++ sorted.py
@@ -1,11 +1,11 @@
 ﻿import requests
 
 url = ('https://newsapi.org/v2/everything?'
-       'q=Apple&'
+       'q=Microsoft&'
        'from=2019-05-12&'
        'sortBy=popularity&'
        'apiKey=11a7d4ea5ca44a99838403e265a62c16')
 
 response = requests.get(url)
 
-print (r.json)
+print r.json
