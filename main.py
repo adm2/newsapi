42d3f68d1f12384cbd2e2f2886a5b8699c663d65
Sergey Pashkov <pashkov.s.y@gmail.com>
2019-05-12 16:33:49 +0500

    Какие-то доработки

--- main.py
+++ main.py
@@ -22,8 +22,13 @@ newsapi = NewsApiClient(api_key='11a7d4ea5ca44a99838403e265a62c16')
 #                                      page=2)
 
 # /v2/sources
-# Поиск по категориям
-def category(select):
+# Получить последние публикации по списку категорий.
+def category(select, lang):
+	if lang == 1:
+		language = 'ru'
+	elif lang == 2:
+		language = 'en'
+
 	if select == 1:
 		category = 'business'
 	elif select == 2:
@@ -37,24 +42,35 @@ def category(select):
 	elif select == 6:
 		category = 'technology'
 	top_headlines = newsapi.get_top_headlines(category=category,
-											  language='ru',
-											  country='ru')
+											  language=language,
+											  )
 	print (top_headlines)
 
-select = int(input('Выберете категорию \n 1 - Бизнес \n 2 - Развлечения \n 3 - Основные \n 4 - Наука \n 5 - Спорт \n 6 - Технологии \n'))
-
-category(select)
-
-
-
-
-# Поиск по ключевым словам
+# Получить последние публикации по списку ключевых слов.
 def description():
-	pass
+	all_articles = newsapi.get_everything(q='bitcoin',
+                                          sources='bbc-news,the-verge',
+                                          domains='bbc.co.uk,techcrunch.com',
+                                          language='en',
+                                          sort_by='relevancy',
+                                          page=2)
+    print (all_articles)
 
-# Поиск по запросу
+# Поиск публикаций по запросу.
 def searh_public():
 	pass
+
+user_input = int(input('Выберете пункт в меню \n 1 - Последние публикации по списку категорий \n 2 - Последние публикации по списку ключевых слов \n 3 - Поиск публикаций по запросу \n'))
+
+if user_input == 1: # Последние публикации по списку категорий
+    input_category = int(input('Выберете категорию \n 1 - Бизнес \n 2 - Развлечения \n 3 - Основные \n 4 - Наука \n 5 - Спорт \n 6 - Технологии \n'))
+    input_language = int(input('Выберете язык \n 1 - Русский \n 2 - Английский \n'))
+    category(input_category, input_language)
+elif user_input == 2: # Последние публикации по списку ключевых слов
+	pass
+elif user_input == 3: # Поиск публикаций по запросу
+	pass
+
 
 #sources = newsapi.get_sources()
 
